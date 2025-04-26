827dcd4a511f91753b20a1427c49a8db6a81eb25
Griffin <53449896+ElectroArtifex@users.noreply.github.com>
2025-04-26 15:33:03 -0500

    fixed heat and cooling for items

--- src/main/java/net/electroartfiex/ncadds/item/ModItems.java
+++ src/main/java/net/electroartfiex/ncadds/item/ModItems.java
@@ -2,7 +2,6 @@ package net.electroartfiex.ncadds.item;
 
 import net.electroartfiex.ncadds.NCAdds;
 import net.minecraft.world.item.Item;
-import net.minecraft.world.item.ItemStack;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -19,13 +18,13 @@ public class ModItems {
     public static final RegistryObject<Item> FIRE_OPAL = ITEMS.register("fire_opal",
             () -> new Item(new Item.Properties()));
     public static final RegistryObject<Item> FIRE_OPAL_AMULET = ITEMS.register("fire_opal_amulet",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> GLACIAL_AQUAMARINE_AMULET = ITEMS.register("glacial_aquamarine_amulet",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> HEATSTONE_AMULET = ITEMS.register("heatstone_amulet",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
     public static final RegistryObject<Item> EIS_AMULET = ITEMS.register("eis_amulet",
-            () -> new Item(new Item.Properties()));
+            () -> new Item(new Item.Properties().stacksTo(1)));
 
     public static void register(IEventBus eventBus) {
 
